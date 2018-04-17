e01856cff9e5afcffd186ffd3b294f8ece31b812
LucasufbaCC <lucassouza@dcc.ufba.br>
2018-04-16 22:31:29 -0300

    ajustes do icons precisam ser feitos

--- app/home/home.module.ts
+++ app/home/home.module.ts
@@ -3,11 +3,14 @@ import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { TNSFontIconModule } from 'nativescript-ngx-fonticon';
 
 @NgModule({
   imports: [
     NativeScriptModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    TNSFontIconModule.forRoot({ 
+      'mdi':'material-design-icons.css'})
   ],
   declarations: [
     HomeComponent
